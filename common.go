b72f7ba94375651a7a4999671a5f4682ef7f91fd
Marcello <lovewy123@gmail.com>
2020-05-18 08:06:50 +0800

    增加移除slice中相同元素函数

--- common.go
+++ common.go
@@ -53,3 +53,18 @@ func RandStr(length int) []byte {
 
 	return b
 }
+
+// 移除 []string 中的相同元素
+func RemoveSameFromStringSlice(slice []string) []string {
+
+	var result []string
+	tmp := map[string]byte{} // 存放不重复主键
+	for _, s := range slice {
+		l := len(tmp)
+		tmp[s] = 0
+		if len(tmp) != l { // 加入map后，map长度变化，则元素不重复
+			result = append(result, s)
+		}
+	}
+	return result
+}
