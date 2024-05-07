67b36061444029a8c8da15728d9112e6b6e69ffe
Marcello <volibearw@gmail.com>
2024-05-07 19:56:22 +0800

    update unit test

--- tools/xml_test.go
+++ tools/xml_test.go
@@ -2,6 +2,7 @@ package tools
 
 import (
 	"encoding/xml"
+	"reflect"
 	"testing"
 )
 
@@ -26,7 +27,6 @@ func TestXMLMarshalString(t *testing.T) {
 		args args
 		want string
 	}{
-		// TODO: Add test cases.
 		{
 			name: "test",
 			args: args{
@@ -52,6 +52,57 @@ func TestXMLMarshalString(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := XMLMarshalString(tt.args.v); got != tt.want {
 				t.Errorf("XMLMarshalString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestXMLMarshalByte(t *testing.T) {
+	type args struct {
+		v interface{}
+	}
+
+	type Person struct {
+		XMLName xml.Name `xml:"root"`
+		Name    string   `xml:"name"`
+		Age     int      `xml:"age"`
+	}
+
+	type School struct {
+		Student string `xml:"student"`
+		Teacher string `xml:"teacher"`
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: "test",
+			args: args{
+				v: Person{
+					Name: "bob",
+					Age:  18,
+				},
+			},
+			want: []byte(`<root><name>bob</name><age>18</age></root>`),
+		},
+		{
+			name: "test1",
+			args: args{
+				v: &School{
+					Student: "bob",
+					Teacher: "Lily",
+				},
+			},
+			want: []byte(`<School><student>bob</student><teacher>Lily</teacher></School>`),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := XMLMarshalByte(tt.args.v); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("XMLMarshalByte() = %v, want %v", got, tt.want)
 			}
 		})
 	}
