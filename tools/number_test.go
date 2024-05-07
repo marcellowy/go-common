af36b71560eb815efae7eab1ab9c0a8bf4411909
Marcello <volibearw@gmail.com>
2024-05-07 11:23:10 +0800

    update unit test

---
+++ tools/number_test.go
@@ -0,0 +1,44 @@
+package tools
+
+import "testing"
+
+func TestIsNumber(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test1",
+			args: args{
+				s: "123",
+			},
+			want: true,
+		},
+		{
+			name: "test2",
+			args: args{
+				s: "abc",
+			},
+			want: false,
+		},
+		{
+			name: "test3",
+			args: args{
+				s: "1bb",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsNumber(tt.args.s); got != tt.want {
+				t.Errorf("IsNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
