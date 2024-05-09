e7f71259e0404225fa6a02b1afb97caabacf669c
Marcello <volibearw@gmail.com>
2024-05-09 14:43:37 +0800

    add StringToBytes/BytesToString

--- tools/bytes.go
+++ tools/bytes.go
@@ -2,10 +2,22 @@
 // Copyright 2023 marcello<volibearw@gmail.com>. All rights reserved.
 package tools
 
+import "unsafe"
+
 // ReverseByte reverse []byte
 func ReverseByte(s []byte) []byte {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}
 	return s
+}
+
+// StringToBytes converts string to a byte slice.
+func StringToBytes(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(&s))
+}
+
+// BytesToString converts byte to a string
+func BytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
 }
--- tools/bytes_test.go
+++ tools/bytes_test.go
@@ -3,6 +3,7 @@ package tools
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -10,5 +11,59 @@ func TestReverseByte(t *testing.T) {
 	var a = []byte("ab")
 	if bytes.Compare(ReverseByte(a), []byte("ba")) != 0 {
 		t.Error("err")
+	}
+}
+
+func TestStringToBytes(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test",
+			args: args{
+				s: "abc",
+			},
+			want: []byte("abc"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := StringToBytes(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("StringToBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBytesToString(t *testing.T) {
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test",
+			args: args{
+				b: []byte("abc"),
+			},
+			want: "abc",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BytesToString(tt.args.b); got != tt.want {
+				t.Errorf("BytesToString() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
