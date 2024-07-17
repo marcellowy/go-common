1a13a43ab731def96dd3459a718315a11e36c648
Marcello <volibearw@gmail.com>
2024-07-17 14:09:34 +0800

    add GBKToUTF8

--- os/vstrings/string.go
+++ os/vstrings/string.go
@@ -1,9 +1,25 @@
 package vstrings
 
-import "strings"
+import (
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io"
+	"strings"
+)
 
 // ReplaceLineBreaks 移除\r, \r\n, \n,
 func ReplaceLineBreaks(s string) string {
 	r := strings.NewReplacer("\r", "", "\n", "")
 	return r.Replace(s)
+}
+
+// GBKToUTF8 GBKToUTF8
+func GBKToUTF8(s string) string {
+	var err error
+	reader := transform.NewReader(strings.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	var sByte []byte
+	if sByte, err = io.ReadAll(reader); err == nil {
+		s = string(sByte)
+	}
+	return s
 }
