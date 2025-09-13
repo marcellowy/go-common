dd90ba63262072269a825230c77b8e2e1ac1c538
Marcello <lovewy123@gmail.com>
2025-09-13 14:44:21 +0800

    optimize logger

--- gogf/vlog/log.go
+++ gogf/vlog/log.go
@@ -3,6 +3,7 @@ package vlog
 
 import (
 	"context"
+
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/glog"
 )
@@ -18,33 +19,33 @@ func SetLogger(log *glog.Logger) {
 }
 
 func Info(ctx context.Context, v ...interface{}) {
-	logger.Skip(1).Line(true).Info(ctx, v...)
+	logger.Skip(1).Line().Info(ctx, v...)
 }
 
 func Infof(ctx context.Context, format string, v ...interface{}) {
-	logger.Skip(1).Line(true).Infof(ctx, format, v...)
+	logger.Skip(1).Line().Infof(ctx, format, v...)
 }
 
 func Error(ctx context.Context, v ...interface{}) {
-	logger.Skip(1).Line(true).Error(ctx, v...)
+	logger.Skip(1).Line().Error(ctx, v...)
 }
 
 func Errorf(ctx context.Context, format string, v ...interface{}) {
-	logger.Skip(1).Line(true).Errorf(ctx, format, v...)
+	logger.Skip(1).Line().Errorf(ctx, format, v...)
 }
 
 func Debug(ctx context.Context, v ...interface{}) {
-	logger.Skip(1).Line(true).Debug(ctx, v...)
+	logger.Skip(1).Line().Debug(ctx, v...)
 }
 
 func Debugf(ctx context.Context, format string, v ...interface{}) {
-	logger.Skip(1).Line(true).Debugf(ctx, format, v...)
+	logger.Skip(1).Line().Debugf(ctx, format, v...)
 }
 
 func Warning(ctx context.Context, v ...interface{}) {
-	logger.Skip(1).Line(true).Warning(ctx, v...)
+	logger.Skip(1).Line().Warning(ctx, v...)
 }
 
 func Warningf(ctx context.Context, format string, v ...interface{}) {
-	logger.Skip(1).Line(true).Warningf(ctx, format, v...)
+	logger.Skip(1).Line().Warningf(ctx, format, v...)
 }
